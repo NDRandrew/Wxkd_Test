c626c21dec4a3b4808dc75d2e319e291ddc2f562
Andrew <50187249+NDRandrew@users.noreply.github.com>
2025-08-22 15:11:01 -0300

    Atualizar o test.php

--- test.php
+++ test.php
@@ -1,21 +1,30 @@
-<script type="text/javascript">
-                // Make ACC_FLAG data available to JavaScript for each row
-                var accFlagData = {
-                    <?php
+<!-- EXISTING contractChaves script -->
+            <script type="text/javascript">
+                var contractChaves = [<?php
+                    $first = true;
+                    foreach (array_keys($contractChavesLookup) as $chave) {
+                        if (!$first) echo ', ';
+                        echo "'" . addslashes($chave) . "'";
+                        $first = false;
+                    }
+                ?>];
+                console.log(contractChaves)
+            </script>
+            
+            <!-- FIXED accFlagData script -->
+            <script type="text/javascript">
+                var accFlagData = {<?php
                     if (is_array($tableData) && !empty($tableData)) {
                         $first = true;
                         foreach ($tableData as $index => $row) {
-                            if (!$first) echo ', ';
+                            if (!$first) echo ',';
                             $chaveLoja = isset($row['Chave_Loja']) ? $row['Chave_Loja'] : '';
                             $accFlag = isset($row['ACC_FLAG']) ? $row['ACC_FLAG'] : '0';
-                            echo "'" . addslashes($chaveLoja) . "': '" . addslashes($accFlag) . "'";
+                            echo "'" . addslashes($chaveLoja) . "':'" . addslashes($accFlag) . "'";
                             $first = false;
                         }
                     }
-                    ?>
-                };
-                
-                // Make it globally available
+                ?>};
                 window.accFlagData = accFlagData;
                 console.log('ACC_FLAG data loaded:', accFlagData);
             </script>
