987faf2a30e4f44c6260f15ec5ff70e94b2b3c1e
Andrew <50187249+NDRandrew@users.noreply.github.com>
2025-09-09 15:42:25 -0300

    Atualizar o test.php

--- test.php
+++ test.php
@@ -1,9 +1,7 @@
-<!-- pom.xml -->
 <?xml version="1.0" encoding="UTF-8"?>
 <project xmlns="http://maven.apache.org/POM/4.0.0"
          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 
-         http://maven.apache.org/xsd/maven-4.0.0.xsd">
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
     <modelVersion>4.0.0</modelVersion>
     
     <groupId>com.example</groupId>
@@ -32,28 +30,3 @@
         </plugins>
     </build>
 </project>
-
-
------------
-
-
-public class CursorPosition {
-    static {
-        System.loadLibrary("pcshll32");
-    }
-    
-    public static void main(String[] args) {
-        int[] pos = new int[2];
-        int result = hllapi(13, "", 0, pos); // Query cursor position
-        
-        if (result == 0) {
-            int row = (pos[0] / 80) + 1;
-            int col = (pos[0] % 80) + 1;
-            System.out.println("Line: " + row + ", Column: " + col);
-        } else {
-            System.err.println("HLLAPI Error: " + result);
-        }
-    }
-    
-    private static native int hllapi(int func, String data, int len, int[] pos);
-}
